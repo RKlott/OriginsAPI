51a63ac760f3b08ef91b5b9c1221844fb5dd848c
RKlott <49999635+RKlott@users.noreply.github.com>
2022-04-11 02:49:32 -0400

    Update Main.java

--- [Origins] API/src/Origins/Main.java
+++ [Origins] API/src/Origins/Main.java
@@ -3,7 +3,7 @@ package Origins;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-//on this: XlurZ, Offdav, Klott, Shinmi42.
+
 	public Main instance;
 	
 	@Override
